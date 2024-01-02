3048a50b2a2597e77a76245bdb65310f8273eb13
Ada <the.nostra.tymus@gmail.com>
2024-01-01 22:25:54 -0500

    updated detector

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -93,8 +93,6 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
 
     // perform feature description
     double t = (double)cv::getTickCount();
-    a
-    extractor->detect(img, keypoints, descriptors);
     extractor->compute(img, keypoints, descriptors);
     t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
     cout << descriptorType << " descriptor extraction in " << 1000 * t / 1.0 << " ms" << endl;
