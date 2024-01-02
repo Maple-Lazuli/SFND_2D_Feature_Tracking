2af0234ba626b80ebe22c286da1488f52ce21605
Ada <the.nostra.tymus@gmail.com>
2024-01-01 23:01:10 -0500

    updated detector

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -77,7 +77,7 @@ int main(int argc, const char *argv[])
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
         //######################################UPDATE#####################################################################
-        string detectorType = "SHITOMASI"; //######################################UPDATE#####################################################################
+        string detectorType = "HARRIS"; //######################################UPDATE#####################################################################
         //######################################UPDATE#####################################################################
         bool bVis = false;
 
@@ -145,7 +145,7 @@ int main(int argc, const char *argv[])
 
         cv::Mat descriptors;
         //######################################UPDATE#####################################################################
-        string descriptorType = "AKAZE"; //BRISK, BRIEF, ORB, FREAK, AKAZE, SIFT
+        string descriptorType = "SIFT"; //BRISK, BRIEF, ORB, FREAK, AKAZE, SIFT
         //######################################UPDATE#####################################################################
 
 
